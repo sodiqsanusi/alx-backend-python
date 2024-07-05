50020703d4544862a6146cd9db39973d1393bafc
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-07-05 12:40:12 +0000

    task: annotate variables in a python module

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Check on how you can annotate variables in a Python module
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
