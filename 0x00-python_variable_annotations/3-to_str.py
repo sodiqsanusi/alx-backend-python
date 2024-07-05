8dde61c9f11b2e99ee70c12b89da1c63409a72ef
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-07-05 12:31:55 +0000

    task: more on type annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+More tests on type annotations. This time, with a float-string function combo
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a provided float
+    """
+    return (str(n))
