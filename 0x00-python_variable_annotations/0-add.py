0589f76ce3a6db7b6781a69af1c6c5a149f52220
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-07-05 12:03:43 +0000

    task: create a type-annotated function in python

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A Python module that tests type annotation in a Python codebase
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two given numbers
+    """
+    return (a + b)
