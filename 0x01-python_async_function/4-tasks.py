1f5f37da1dd9a3c2bf2fcaf261af1da7d2b46508
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-07-08 19:22:13 +0000

    task: convert a previously built function into a new one based on present requirements

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+Run multiple asynchronous functions concurrently
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Run the 'wait_random' function n times, and get all its values
+    asynchronously into a list that will be returned
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    final_list = await asyncio.gather(*tasks)
+    return (final_list)
