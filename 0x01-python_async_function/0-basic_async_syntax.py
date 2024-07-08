f565a6076df93ed884160d60e9588250af2288d1
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-07-08 12:40:30 +0000

    task: create an async function that generates a random wait time, and delays for that time

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Create an async function that can be awaited by an asyncio method
+"""
+import random
+from time import sleep
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Gets a random number btw. 0 and inputted param, sleeps for the time
+    gotten from the random number
+    """
+    wait_time = random.uniform(0, max_delay)
+    sleep(wait_time)
+    return (wait_time)
