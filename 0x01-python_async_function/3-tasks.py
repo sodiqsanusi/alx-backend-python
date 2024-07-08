d8f0944c5fbffd82acb8e6e8ba9f7aa08284fb39
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-07-08 19:17:47 +0000

    task: Return an async task from a non-synchronous function

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+Using a regular function, return an asynchronous task
+"""
+import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Return an async task using asyncio
+    """
+    return (asyncio.create_task(wait_random(max_delay)))
