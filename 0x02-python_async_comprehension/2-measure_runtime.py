bc5adee166d0115b551148631341d4f1cde784fc
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-07-09 22:37:06 +0000

    bug: i hope this is it

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+
 """
 Measure the runtime of an async function
 """
+
 import asyncio
 from time import time
 from typing import Callable
+
 async_comprehension: Callable = __import__(
         '1-async_comprehension'
 ).async_comprehension
@@ -14,7 +17,9 @@ async def measure_runtime() -> float:
     """
     Run some asynchronous functions and calculate their execution time
     """
+
     start_time: float = time()
+
     await asyncio.gather(
             async_comprehension(), async_comprehension(),
             async_comprehension(), async_comprehension()
